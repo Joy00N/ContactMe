536fd594993b71ae9b6139c90c1b929fc464a989
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-08-21 23:15:17 -0400

    user setup boilerplate

--- src/main/java/com/ccms/contactme/model/User.java
+++ src/main/java/com/ccms/contactme/model/User.java
@@ -1,8 +1,23 @@
 package com.ccms.contactme.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
     @Id
     private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
 }
---
+++ src/main/java/com/ccms/contactme/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ccms.contactme.repositories;
+
+import com.ccms.contactme.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
