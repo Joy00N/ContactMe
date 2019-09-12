be5e9bfec46551af40def0911d8be64d82bbe075
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-09-12 02:23:48 -0400

    sign in, sign up, lense entry page are connected & new user gets saved in db.
    # Conflicts:
    #	frontend/src/App.js

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,28 +1,30 @@
-import React from "react";
+import React, {Component} from 'react'
 
-const Users = ({users}) => {
-    return (
-        <div>
-            <table>
-                <tbody>
-                <tr>
-                    <td span={6}>Users List</td>
+class Users extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: this.props.users
+        }
+    }
+
+    render() {
+        let isUserExist = this.props.users.length === 0 ? false : true;
+
+        return (
+            this.state.users.map((user) => (
+                <tr key={user.id}>
+                    {/*<td span={6}>{user.id}</td>*/}
+                    <td span={6}>{user.username}</td>
+                    <td span={6}>{user.password}</td>
+                    <td span={6}>{user.firstname}</td>
+                    <td span={6}>{user.lastname}</td>
+                    <td span={6}>{user.email}</td>
+                    <td span={6}>{user.phone}</td>
                 </tr>
-                {users.map((user) => (
-                    <tr key={user.id}>
-                        {/*<td span={6}>{user.id}</td>*/}
-                        <td span={6}>{user.username}</td>
-                        <td span={6}>{user.password}</td>
-                        <td span={6}>{user.firstname}</td>
-                        <td span={6}>{user.lastname}</td>
-                        <td span={6}>{user.email}</td>
-                        <td span={6}>{user.phone}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    )
-};
+            ))
+        )
+    }
+}
 
 export default Users
